67e355904202f78116a3c5963846414f96c1df76
小强同学 <765187944@qq.com>
2022-07-23 12:51:11 +0800

    update

--- train_utils/net_config.py
+++
@@ -1,9 +0,0 @@
-# implement net hyper-parameters config
-__all__ = ['args']
-
-args = {'num_epochs': 400,
-        'optimizer': 'SGD',
-        'optimizer_para': {'lr': 1e-2, 'momentum': 0.9, 'weight_decay': 1e-4},
-        'scheduler': 'CosineAnnealingLR',
-        'scheduler_para': {'T_max': 400},
-        'batch_size': 32}
